e4cb3ed657cd6504e0160bb7687b0ad402cbcf23
김동건(Donggun Kim) <donggunkim@hanwha.com>
2019-03-25 19:26:08 +0900

    setting up development environment

---
+++ src/components/Main.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Header/>
+    <Worklist/>
+  </div>
+</template>
+
+<script>
+import Header from './Header'
+import Worklist from './Worklist'
+
+export default {
+  name: 'Main',
+  templates: {
+    Header,
+    Worklist
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Worklist.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="work-list" v-for="list in lists" :key="list.work_NO">
+      <div class="work-list-title">{{ list.work_NM }}</div>
+      <span class="work-list-left-align">작업자 : {{ list.pchr_EMPN_NM }} / {{ list.wrk_REQ_RESN_NM }}</span>
+      <span class="work-list-right-align">{{ list.work_PRGS_STAT_NM }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Worklist',
+  data () {
+    return {
+      lists: []
+    }
+  },
+  created () {
+    this.$http.get('/api/corp/m/M_WORK_WORK_INFO_LIST_07.do').then(resp => {
+      this.lists = resp.data.response
+      console.log(resp)
+    })
+  }
+}
+</script>
+
+<style scoped>
+.work-list {
+  display: inline-block;
+  vertical-align: middle;
+  width: 100%;
+  height: 60px;
+  opacity: 0.88;
+  background: #FFFFFF;
+  border-bottom: 1px solid #000000;
+}
+
+.work-list-title {
+  font-size: 14px;
+  width: 100%;
+  padding-top: 7px;
+  padding-bottom: 7px;
+  margin-left: 5px;
+  float: left;
+  white-space: nowrap;
+}
+
+.work-list-left-align {
+  font-size: 14px;
+  margin-left: 5px;
+  float: left;
+}
+
+.work-list-right-align {
+  font-size: 14px;
+  margin-right: 5px;
+  float: right;
+}
+</style>
